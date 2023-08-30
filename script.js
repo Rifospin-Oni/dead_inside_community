dc36266955ff4f64cbf05a433218d334467bc155
RifospinOni <lavazgiza2@gmail.com>
2023-08-30 13:02:10 +0300

    added files and added functions

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-const blurpanel = document.querySelector('.blur');
-console.log(blurpanel);
+const blurpanel = document.querySelector('.blur')
+let clickSound = new Audio("click1.mp3")
+let navigation = document.querySelector('.navigation')
+let hoverSound = new Audio("hover.mp3")
 function blur()
 {
     document.querySelector('.main').classList.toggle('blurred');
+    clickSound.play()
+    clickSound.volume= 0.2
+    blurpanel.classList.toggle('button_blurred');
 }
-
+function navisound()
+{
+    hoverSound.play()
+    console.log("hovered")
+    hoverSound.volume =0.2
+}
+navigation.addEventListener("pointerenter", navisound);
 blurpanel.addEventListener('click',blur);
